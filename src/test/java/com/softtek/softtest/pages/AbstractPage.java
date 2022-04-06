4dc524861c3527d41dd8ca211a497797642e1922
miguelvargas <miguel.vargas@autentic.com.co>
2022-04-06 10:59:50 -0500

    base page creation

---
+++ src/test/java/com/softtek/softtest/pages/AbstractPage.java
@@ -0,0 +1,26 @@
+package com.softtek.softtest.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class AbstractPage {
+    private static final Duration MAX_DURATION = Duration.ofSeconds(30);
+    protected final WebDriver driver;
+    protected final Wait<WebDriver> wait;
+
+    public AbstractPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, MAX_DURATION);
+        PageFactory.initElements(driver, this);
+    }
+
+    public AbstractPage(WebDriver driver, Wait<WebDriver> wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+}
